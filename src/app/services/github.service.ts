40ad0321162b83c8383fbe2dc58ebdd00581cd66
Felipe Rojas <juancendales@periferia-it.com>
2024-09-15 18:43:05 -0500

    Remove unused

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Octokit } from "octokit";
-import { env } from 'process';
-import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
